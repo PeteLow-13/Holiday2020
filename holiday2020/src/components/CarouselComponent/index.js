f1690cae549e6cd64e283d8bd4104b2f9ee755d4
Pete Low <pedritolow@gmail.com>
2021-01-05 21:27:55 -0800

    added more images and got carousel to display them

--- holiday2020/src/components/CarouselComponent/index.js
+++ holiday2020/src/components/CarouselComponent/index.js
@@ -1,72 +1,105 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+import './style.css';
+import joaquin from '../../assets/joaquin.jpeg';
+import alexie from '../../assets/alexie.jpeg';
+import janda from '../../assets/janda.jpeg';
+import chauncey from '../../assets/chauncey.jpeg';
+import maria from '../../assets/maria.jpeg';
+import pete from '../../assets/pete.jpeg';
+import alexieOrcas from '../../assets/alexieOrcas.jpeg';
+import florida from '../../assets/florida.jpeg';
 
 
+const carouselImages = [joaquin, alexie, janda, maria, pete, florida, alexieOrcas, chauncey];
 
 export default function CarouselComponent() {
     return (
-        <div>
-            <Carousel>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=First slide&bg=373940"
-                    alt="Joaquin"
-                    />
-                    <Carousel.Caption>
-                    <h3>Joaquin</h3>
-                    <p>This year Joquin made the transition to online learning at Rainier Prep. He made the first quarter Honor Roll this fall and can't wait for sports to start again.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=Second slide&bg=282c34"
-                    alt="Alexie"
-                    />
+        <Carousel>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src= {carouselImages[0]}
+                alt="Joaquin"
+                />
+                <Carousel.Caption>
+                <h3>Joaquin</h3>
+                <p>This year Joquin made the transition to online learning at Rainier Prep. He made the first quarter Honor Roll this fall, played lots of video games, and can't wait for sports to start again.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src= {carouselImages[1]}
+                alt="Alexie"
+                />
 
-                    <Carousel.Caption>
-                    <h3>Alexie</h3>
-                    <p>Alexie is enjoying being in Ms Deuel's online classroom at Leschi Elementary. She can't wait for school to start in person again. She is looking forward to playing softball this spring.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=Third slide&bg=20232a"
-                    alt="Maria"
-                    />
+                <Carousel.Caption>
+                <h3>Alexie</h3>
+                <p>Alexie is enjoying being in Ms Deuel's online classroom at Leschi Elementary. She can't wait for school to start in person again. She is looking forward to playing softball this spring.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src={carouselImages[3]}
+                alt="Maria"
+                />
 
-                    <Carousel.Caption>
-                    <h3>Maria</h3>
-                    <p>Maria kept Madres's kitchen open as a home meal delivery company from March until August when the owners decided to shut down until conditions improve. She is currently is starting classes at Seattle Central College this quarter working towards the Graphic design program.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=Third slide&bg=20232a"
-                    alt="Pete"
-                    />
+                <Carousel.Caption>
+                <h3>Maria</h3>
+                <p>Maria kept Madres's kitchen open as a home meal delivery company from March until August when the owners decided to shut down until conditions improve. She is currently is starting classes at Seattle Central College this quarter working towards the Graphic design program.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src={carouselImages[4]}
+                alt="Pete"
+                />
 
-                    <Carousel.Caption>
-                    <h3>Pete</h3>
-                    <p>My long time employer shut down this year. In June I began a Web Development Bootcamp through the University of Washington which I successfully completed in December. I built this site using skills I learned and am searching for a job in the industry now.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=Third slide&bg=20232a"
-                    alt="Family"
-                    />
+                <Carousel.Caption>
+                <h3>Pete</h3>
+                <p>My long time employer shut down this year. In June I began a Web Development Bootcamp through the University of Washington which I successfully completed in December. I built this site using skills I learned and am searching for a job in the industry now.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src={carouselImages[5]}
+                alt="Family"
+                />
 
-                    <Carousel.Caption>
-                    <h3>Family</h3>
-                    <p>We've mostly been hanging out close to home but we did make a trip up to Orcas Island this summer. We have Missed all of you very much and hope to be able to see you outside of screens this year. Everyone please stay safe and healthy and hopefully we'll see you in 2021!</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
-        </div>
+                <Carousel.Caption>
+                <h3>Florida</h3>
+                <p>We went to Florida in February before the lockdowns started.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src={carouselImages[6]}
+                alt="Family"
+                />
+
+                <Carousel.Caption>
+                <h3>Orcas</h3>
+                <p>We've mostly been hanging out close to home but we did make a trip up to Orcas Island this summer.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src={carouselImages[7]}
+                alt="Family"
+                />
+
+                <Carousel.Caption>
+                <h3>Stay Safe!</h3>
+                <p> We have Missed all of you very much and hope to be able to see you outside of screens this year. Everyone please stay safe and healthy and hopefully we'll see you in 2021!</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+        
     )
 }
--- holiday2020/src/components/Header/index.js
+++ holiday2020/src/components/Header/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
-import Button from 'react-bootstrap/Button';
-import './style.css'
+import './style.css';
 
 
 export default function Header() {
@@ -12,12 +11,6 @@ export default function Header() {
                 <p>
                  We hope you and yours are having a wonderful holiday season. We built this site instead of a holiday card as an update on what we've all be up to during this crazy year!
                 </p>
-                {/* <p>
-                <Button variant="primary">Joaquin</Button>
-                <Button variant="primary">Alexie</Button>
-                <Button variant="primary">Maria</Button>
-                <Button variant="primary">Pete</Button>
-                </p> */}
             </Jumbotron>
         </div>
     )
